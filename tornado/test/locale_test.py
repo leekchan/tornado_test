3a2dcaf20c246727bdff49138b66a369ee7fdb4b
Lee Kyoung chan <leekchan@gmail.com>
2014-09-24 20:33:34 +0900

    Add test_format_day

--- tornado/test/locale_test.py
+++ tornado/test/locale_test.py
@@ -95,5 +95,5 @@ class EnglishTest(unittest.TestCase):
     def test_format_day(self):
         locale = tornado.locale.get('en_US')
         date = datetime.datetime(2013, 4, 28, 18, 35)
-        self.assertEqual(locale.format_day(date=date, dow=True), '')
-        self.assertEqual(locale.format_day(date=date, dow=False), '')
+        self.assertEqual(locale.format_day(date=date, dow=True), 'Sunday, April 28')
+        self.assertEqual(locale.format_day(date=date, dow=False), 'April 28')
